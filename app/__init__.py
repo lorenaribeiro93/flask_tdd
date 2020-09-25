ce022391d02fc2c9410ed5c5c2bb313d9d88fad2
Lorena Ribeiro <59025920+lorenaribeiro93@users.noreply.github.com>
2020-09-25 16:07:36 -0300

    Template inicial de login

---
+++ app/__init__.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template, template_rendered
+
+
+def create_app():
+    app = Flask(__name__)
+
+    @app.route('/login')
+    def login():
+        return render_template('login.html')
+
+    return app
+
